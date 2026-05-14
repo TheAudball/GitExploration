93dca6fa6cfa15f889b0eea9c9b433c62bc01f2d
Audrey Plunkett <alphacataudrey@gmail.com>
2026-05-14 11:21:23 -0500

    added list ordering functionality

--- DirectoryStructure.py
+++ DirectoryStructure.py
@@ -2,16 +2,41 @@ import os
 import sys
 
 
-def buildDirStruct(rootPath:str,optionFileName:str="options.txt"):
+def buildDirStruct(rootPath:str,optionFileName:str="options.py"):
     dirstruct = {"name":rootPath}
     directory = os.scandir(rootPath)
     entryList = []
     for entry in directory:
         if entry.is_dir():
-            dirstruct[entry.name] = buildDirStruct(os.path.join(rootPath,entry.name),optionFileName)
-        entryList.append(entry.name)
+            dirstruct[entry.name] = buildDirStruct(os.path.join(rootPath,entry.name),
+                                                   optionFileName)
+        if entry.name == optionFileName:
+            dirstruct["options"] = entry.name
+        else:
+            entryList.append(entry.name)
     dirstruct["entries"] = entryList
     return dirstruct
+
+def OrderEntries(entryList,orderList):
+    sortedList = []
+    for item in orderList:
+        if item == '*':
+            for entry in entryList:
+                if entry in orderList:
+                    pass
+                else:
+                   sortedList.append(entry)
+        elif item not in entryList:
+            pass
+        else:
+            sortedList.append(item)
+    for entry in entryList:
+        if entry not in sortedList:
+            sortedList.append(entry)
+    return sortedList
+            
+
+
 
 
 if __name__ == "__main__":
